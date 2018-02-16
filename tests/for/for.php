f8f1580093e24489f46a0d79e6a0e3ab55a5fe43
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2018-02-16 08:28:26 +0100

    Handle multiple expressions in for loops
    Fixes #43

--- tests/for/for.php
+++ tests/for/for.php
@@ -6,3 +6,7 @@ for ($i = 0; $i <= 5; $i++) {
 for ($really_really_long_variable_name = 0; $really_really_long_variable_name <= 5; $really_really_long_variable_name++) {
   $test = $really_really_long_variable_name;
 }
+
+for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
+
+for ($really_really_long_variable_name = 0, $and_something_else = 0; $really_really_long_variable_name <= 5; $really_really_long_variable_name++, print $really_really_long_variable_name, $even_more_stuff++);
