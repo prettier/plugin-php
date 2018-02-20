43a9356a196668654c4ae2063d6b4856565363a8
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-21 00:28:02 +0300

    tests: cast

---
+++ tests/cast/cast.php
@@ -0,0 +1,15 @@
+<?php
+
+$bar = (int) $foo;
+$bar = (integer) $foo;
+$bar = (bool) $foo;
+$bar = (boolean) $foo;
+$bar = (float) $foo;
+$bar = (double) $foo;
+$bar = (real) $foo;
+$bar = (array) $foo;
+$bar = (object) $foo;
+$bar = (unset) $foo;
+
+$bar = ( int ) $foo;
+$bar = (  int  )  $foo;
