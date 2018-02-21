dc0c5fb55c7ddb5642177233e53a37f24058229f
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-02-22 00:50:42 +0300

    fix: unnecessary newline between case (#89)

--- src/printer.js
+++ src/printer.js
@@ -1087,7 +1087,7 @@ function printNode(path, options, print) {
         node.test
           ? concat(["case ", path.call(print, "test"), ":"])
           : "default:",
-        indent(concat([line, path.call(print, "body")]))
+        node.body ? indent(concat([line, path.call(print, "body")])) : ""
       ]);
     case "break":
       if (node.level) {
