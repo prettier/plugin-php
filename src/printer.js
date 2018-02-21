415d8281f455c449c4958b55f20c4553790aceb6
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-21 14:15:53 +0300

    fix: don't output extra newline with single open php tag

--- src/printer.js
+++ src/printer.js
@@ -371,7 +371,7 @@ function printStatement(path, options, print) {
           path.call(childrenPath => {
             return printLines(childrenPath, options, print);
           }, "children"),
-          hardline
+          node.children.length ? hardline : ""
         ]);
       }
       case "namespace": {
