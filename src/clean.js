e049f68e4fc50eb4653edbdbfbae9709191bbf79
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-02-26 20:41:58 +0300

    feat: lowercase all keywords (#73)

--- src/clean.js
+++ src/clean.js
@@ -16,10 +16,34 @@ function clean(ast) {
         : { kind: ast.kind, level };
     }
   }
+  // Normalize numbers
   if (ast.kind === "number") {
     ast.value = util.printNumber(ast.value);
 
     return ast;
+  }
+  // All magic constant should be upper case
+  if (ast.kind === "magic") {
+    return ast.value.toUpperCase();
+  }
+
+  // All reserved words should be lowercase case
+  if (ast.kind === "identifier" && typeof ast.name === "string") {
+    const lowerCasedName = ast.name.toLowerCase();
+    const isLowerCase =
+      [
+        "int",
+        "float",
+        "bool",
+        "string",
+        "null",
+        "void",
+        "iterable",
+        "object",
+        "self"
+      ].indexOf(lowerCasedName) !== -1;
+
+    return isLowerCase ? lowerCasedName : ast.name;
   }
 }
 
