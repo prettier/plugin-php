461d4a6deb2e930910b91240b75ebc52f47d7724
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-02-26 18:59:07 +0300

    tests: constant expressions

--- tests/constants/constants.php
+++ tests/constants/constants.php
@@ -1,6 +1,8 @@
 <?php
 define("TEST_CONSTANT", 1);
 const OTHER_TEST_CONSTANT = 2;
+const ONE = 1;
+const TWO = ONE * 2;
 
 $test = TEST_CONSTANT;
 $test = OTHER_TEST_CONSTANT;
@@ -10,8 +12,15 @@ class Constants {
   public const PUBLIC_TEST_CLASS_CONSTANT = 'value';
   protected const PROTECTED_TEST_CLASS_CONSTANT = 'value';
   private const PRIVATE_TEST_CLASS_CONSTANT = 'value';
+  public const THREE = TWO + 1;
+  public const ONE_THIRD = ONE / self::THREE;
+  public const SENTENCE = 'The value of THREE is ' . self::THREE;
 
   public static function testClassConstant() {
     return self::TEST_CLASS_CONSTANT;
+  }
+
+  public function f($a = ONE + self::THREE) {
+    return $a;
   }
 }
