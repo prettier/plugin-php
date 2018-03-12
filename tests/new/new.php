0144b203a88bec70f23a01232e44cdd252f443da
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-03-12 18:37:19 +0300

    tests: new `static` (#203)

--- tests/new/new.php
+++ tests/new/new.php
@@ -7,3 +7,13 @@ $c = new Foo([1, 2, 3], "foo", $variable);
 $bar = 'MyClassName';
 $foo = new $bar;
 $foo = new $bar();
+
+abstract class A
+{
+    public static function create()
+    {
+        $a = new static;
+        $b = new static();
+    }
+
+}
