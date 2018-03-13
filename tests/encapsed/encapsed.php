1cf3370ffe443343dd29dc21d3fa194ee7aa1490
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-03-13 16:41:03 +0300

    fix: indent for `heredoc` and `nowdoc`

--- tests/encapsed/encapsed.php
+++ tests/encapsed/encapsed.php
@@ -8,3 +8,35 @@ $encapsHereDoc2 = <<<STR
 Hello foo $a bar
 new line
 STR;
+echo <<<"FOOBAR"
+Hello World!
+FOOBAR;
+
+$str = <<<'EOD'
+Example of string
+spanning multiple lines
+using nowdoc syntax.
+EOD;
+
+function foo()
+{
+    $string = <<<EOJS
+some text
+should not indent
+EOJS;
+    $string = <<<'EOD'
+Example of string
+spanning multiple lines
+using nowdoc syntax.
+EOD;
+}
+
+class test {
+    public function get_string(){
+        $string = <<<EOJS
+         some text
+should not indent
+EOJS;
+        return $string;
+    }
+}
