5cc9a2d778174bfdc8aa07fd0e197b053425c6b3
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-03-16 17:39:56 +0300

    tests: clone (#226)

--- tests/clone/clone.php
+++ tests/clone/clone.php
@@ -1,2 +1,7 @@
 <?php
 $obj2 = clone $obj1;
+$obj2 = clone  $obj;
+$obj2 = clone
+$obj;
+$obj2 = clone $veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongClass;
+$obj2 = clone $this->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLeadDeveloper;
