ed08cb5e6b4cc04cd58b905318b228d2981f255c
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-03-19 13:28:23 +0300

    feat: improve `clone` output

--- tests/clone/clone.php
+++ tests/clone/clone.php
@@ -5,3 +5,4 @@ $obj2 = clone
 $obj;
 $obj2 = clone $veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongClass;
 $obj2 = clone $this->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLeadDeveloper;
+$OneSecAgo = (clone $Now)->veryVeryVeryVeryVeryVeryVeryVeryLongMethod(new \DateInterval("PT1S"));
