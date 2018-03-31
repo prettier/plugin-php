043fae397b7c74c4acaf1382f47d386c6ff16781
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-03-31 19:15:22 +0300

    fix: print comments in function and method arguments

---
+++ tests/comments/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+function foo(/* comment */ $a /* comment */ = /* comment */ 1 /* comment */): /* comment */ ?string /* comment */ {}
---
+++ tests/comments/method.php
@@ -0,0 +1,5 @@
+<?php
+
+class Foo {
+    public function foo(/* comment */ $a /* comment */ = /* comment */ 1 /* comment */): /* comment */ ?string /* comment */ {}
+}
