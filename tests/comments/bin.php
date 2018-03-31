8ae646343fdda041a9665faeb1df361e604d0473
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-03-31 18:25:30 +0300

    fix: print comments inside `bin` (#282)

---
+++ tests/comments/bin.php
@@ -0,0 +1,11 @@
+<?php
+
+$variable = FormUtil::isEmpty($this->modelData) ||
+    // arrays, countables
+    ((is_array($this->modelData) || $this->modelData instanceof \Countable) && 0 === count($this->modelData)) ||
+    // traversables that are not countable
+    ($this->modelData instanceof \Traversable && 0 === iterator_count($this->modelData));
+
+if (2 /* MB_OVERLOAD_STRING */ & (int) ini_get('mbstring.func_overload')) {
+    mb_internal_encoding('ASCII');
+}
