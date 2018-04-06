5354cd9893d9aaa9e8bc477f3bf5f9a5ff3a2922
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-04-06 18:08:10 +0300

    fix: don't output newline before opening curly brace in `function` node (#336)

--- tests/functions/functions.php
+++ tests/functions/functions.php
@@ -100,3 +100,63 @@ string
 {
     return 'hi';
 }
+
+function the_panel_title(
+    $title = null,
+    $classes = 'panel-title',
+    $data_name = null,
+    $data_livetext = false,
+    $depth = 0,
+    $index = 0
+)
+{
+    if (empty($title) && !is_panel_preview()) {
+        return "";
+    }
+
+    return $title;
+}
+
+$longArgs_noVars = function (
+    $longArgument,
+    $longerArgument,
+    $muchLongerArgument
+) {
+    // body
+};
+
+$noArgs_longVars = function () use (
+    $longVar1,
+    $longerVar2,
+    $muchLongerVar3
+) {
+    // body
+};
+
+$longArgs_longVars = function (
+    $longArgument,
+    $longerArgument,
+    $muchLongerArgument
+) use (
+    $longVar1,
+    $longerVar2,
+    $muchLongerVar3
+) {
+    // body
+};
+
+$longArgs_shortVars = function (
+    $longArgument,
+    $longerArgument,
+    $muchLongerArgument
+) use ($var1) {
+    // body
+};
+
+$shortArgs_longVars = function ($arg) use (
+    $longVar1,
+    $longerVar2,
+    $muchLongerVar3
+) {
+    // body
+};
