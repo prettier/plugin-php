d429a7344580fd7931f5b26c1c182f4a1171222c
Mike Grip <mgrip@wayfair.com>
2018-04-19 17:44:49 -0400

    adding test for if

--- tests/comments/if.php
+++ tests/comments/if.php
@@ -17,3 +17,8 @@ if (1) { // Comment
 if (/* Comments */ true /* Comments */) {
 } elseif (/* Comments */ false /* Comments */) {
 } else {}
+
+
+  if (2 /* MB_OVERLOAD_STRING */ & (int) ini_get('mbstring.func_overload')) {
+      mb_internal_encoding('ASCII');
+  }
