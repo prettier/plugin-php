0b83f6bc5d065f63f507c23b04655dbdc7f0fe06
Christian Zosel <christian@zosel.ch>
2018-05-04 23:11:24 +0200

    Fix placement of comment before elseif

--- tests/comments/if.php
+++ tests/comments/if.php
@@ -1,22 +1,22 @@
 <?php
-// Comment
-if (1) { // Comment
-    // Comment
-    echo "Foo"; // Comment
-    // Comment
+// Comment 1
+if (1) { // Comment 2
+    // Comment 3
+    echo "Foo"; // Comment 4
+    // Comment 5
 }
-// Comment
-elseif (2) { // Comment
-    // Comment
+// Comment 6
+elseif (2) { // Comment 7
+    // Comment 8
     echo "Bar";
-    // Comment
+    // Comment 9
 }
-// Comment
-else { // Comment
-    // Comment
+// Comment 10
+else { // Comment 11
+    // Comment 12
     echo "FooBar";
-    // Comment
-} // Comment
+    // Comment 13
+} // Comment 14
 
 if (/* Comments */ true /* Comments */) {
 } elseif (/* Comments */ false /* Comments */) {
@@ -26,3 +26,11 @@ if (/* Comments */ true /* Comments */) {
 if (2 /* MB_OVERLOAD_STRING */ & (int) ini_get('mbstring.func_overload')) {
   mb_internal_encoding('ASCII');
 }
+
+// this is a comment on an if
+if (false)
+    do_nothing();
+
+// and this is a comment on an elseif
+elseif (false)
+    also_do_nothing();
