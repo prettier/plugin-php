7c9c3dda0bb2b13534756b125a2c7b7a44acce8e
Mike Grip <mgrip@wayfair.com>
2018-06-19 17:50:56 -0400

    fix: shortform for loop breaking comment

--- tests/comments/for.php
+++ tests/comments/for.php
@@ -15,3 +15,11 @@ for ($i = 1; $i <= 10; $i++) { // Comment
 for (/* Comments */ $i /* Comments */ = /* Comments */ 1 /* Comments */; /* Comments */ $i /* Comments */ <= /* Comments */ 10 /* Comments */; /* Comments */ $i++ /* Comments */) {
     echo $i;
 }
+
+foreach([1,2,3] as $i)
+    // this comment will screw things up
+    echo $i;
+
+for($i = 1; $i < 10; $i++)
+    // this comment will screw things up
+    echo $i;
