19256898a7302f49fd6c687284411a9311a0773a
evilebottnawi <sheo13666q@gmail.com>
2018-08-10 20:30:54 +0300

    revert: inline nodes from ast

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,9 @@
 "use strict";
 const engine = require("php-parser");
 
-function inBetweenLines(line, node, nextNode) {
-  return (
-    nextNode && node.loc.end.line <= line && nextNode.loc.start.line > line
-  );
-}
-
 function parse(text) {
+  text = text.replace(/\?>\n<\?/g, "?>\n___PSEUDO_INLINE_PLACEHOLDER___<?");
+
   // initialize a new parser instance
   const parser = new engine({
     parser: {
@@ -22,31 +18,6 @@ function parse(text) {
 
   const ast = parser.parseCode(text);
 
-  // parser doesn't create `inline` node between `?>\n<?`, so we add them manually
-  const missingLinebreaks = ast.tokens.filter((token, index, tokens) => {
-    return (
-      token[0] === "T_CLOSE_TAG" &&
-      token[1] === "?>\n" &&
-      tokens[index + 1] &&
-      tokens[index + 1][0] === "T_OPEN_TAG"
-    );
-  });
-  const lines = missingLinebreaks.map(token => token[2]);
-
-  const astIndices = ast.children.reduce((acc, child, index, children) => {
-    if (lines.find(line => inBetweenLines(line, child, children[index + 1]))) {
-      acc.push(index + 1);
-    }
-    return acc;
-  }, []);
-  astIndices.forEach((i, j) => {
-    ast.children.splice(i + j, 0, {
-      kind: "inline",
-      raw: "\n",
-      value: ""
-    });
-  });
-
   // https://github.com/glayzzle/php-parser/issues/155
   // currently inline comments include the line break at the end, we need to
   // strip those out and update the end location for each comment manually
@@ -60,6 +31,7 @@ function parse(text) {
       comment.loc.end.offset = comment.loc.end.offset - 1;
     }
   });
+
   return ast;
 }
 
