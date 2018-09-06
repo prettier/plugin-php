6d55ce2d29202d60b47e43c41e4b40d5dd6fa022
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-06 21:01:04 +0300

    test: parens return (#582)

---
+++ tests/parens/return.php
@@ -0,0 +1,10 @@
+<?php
+
+return;
+return 1;
+return (1);
+return (1 + 2);
+return ('veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' . 'veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString');
+return ($var1 + $var2);
+return $var ? ($var1 ? 1 : 2) : ($var2 ? 3 : 4);
+return ('veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' ? 'veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' : 'veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString');
