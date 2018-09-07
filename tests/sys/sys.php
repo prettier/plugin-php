2ac65600f629615a449c4871403347dc570e6f78
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-07 13:06:31 +0300

    refactor: use print argument logic for `isset`, `unset` and `empty` nodes (#583)

--- tests/sys/sys.php
+++ tests/sys/sys.php
@@ -14,3 +14,14 @@ isset($test);
 unset($test);
 
 empty($test);
+
+isset($test, $test, $test,$test, $test, $test, $test, $test, $test, $test, $test, $test);
+
+isset($test['foo']);
+isset($a['cake']['a']['b']);
+isset($expected_array_got_string['some_key']);
+isset($expected_array_got_string[0]);
+isset($expected_array_got_string['0']);
+isset($expected_array_got_string[0.5]);
+isset($expected_array_got_string['0.5']);
+isset($expected_array_got_string['0 Mostel']);
