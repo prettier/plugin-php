8d755e2765b2d28b2bc802512a8313d32d65e1ae
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-10 15:31:44 +0300

    test: `continue` and `break` nodes (#595)

---
+++ tests/parens/break.php
@@ -0,0 +1,11 @@
+<?php
+
+break;
+break 1;
+break (1);
+break ((1));
+break (((1)));
+break 2;
+break (2);
+break ((2));
+break (((2)));
---
+++ tests/parens/continue.php
@@ -0,0 +1,11 @@
+<?php
+
+continue;
+continue 1;
+continue (1);
+continue ((1));
+continue (((1)));
+continue 2;
+continue (2);
+continue ((2));
+continue (((2)));
