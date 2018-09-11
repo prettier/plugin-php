b65f6c38c8d6befcbcb9369c87157e2992bbba84
evilebottnawi <sheo13666q@gmail.com>
2018-09-11 18:34:44 +0300

    feat: avoid unnecessary parens around `isset` and `empty` nodes

---
+++ tests/parens/empty.php
@@ -0,0 +1,9 @@
+<?php
+
+empty($var);
+(empty($var));
+
+$var = empty($var);
+$var = (empty($var));
+
+if ((empty($var))) {}
---
+++ tests/parens/isset.php
@@ -0,0 +1,11 @@
+<?php
+
+isset($var);
+(isset($var));
+
+$var = isset($var);
+$var = (isset($var));
+$var = isset($var, $var, $var);
+$var = (isset($var, $var, $var));
+
+if ((isset($var))) {}
