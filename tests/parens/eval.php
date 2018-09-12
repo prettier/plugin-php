b854e4576b084617666677220030b4dad270a38a
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-12 12:25:51 +0300

    feat: avoid parens around eval node (#613)

---
+++ tests/parens/eval.php
@@ -0,0 +1,12 @@
+<?php
+
+eval('return 1;');
+(eval('return 1;'));
+(eval(('return 1;')));
+
+$var = eval('return 1;');
+$var = (eval('return 1;'));
+$var = (eval(('return 1;')));
+
+if (eval('return 1;')) {}
+if ((eval('return 1;'))) {}
