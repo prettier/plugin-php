2c4a6917a1d164cfaaffee554f8cb35c7bb39a51
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-12 12:25:41 +0300

    test: return parens cases (#612)

--- tests/parens/return.php
+++ tests/parens/return.php
@@ -8,3 +8,6 @@ return ('veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' . 'veryVeryVeryVery
 return ($var1 + $var2);
 return $var ? ($var1 ? 1 : 2) : ($var2 ? 3 : 4);
 return ('veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' ? 'veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString' : 'veryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString');
+return static::class . '@' . $method;
+return ($this->customer->paymentService ?? null);
+
