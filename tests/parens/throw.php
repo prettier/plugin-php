428badd323471bea7186fbf4e091cc59790d7514
evilebottnawi <sheo13666q@gmail.com>
2018-09-12 13:43:48 +0300

    test: throw parens

---
+++ tests/parens/throw.php
@@ -0,0 +1,17 @@
+<?php
+
+throw new Exception('Division by zero.');
+throw (new Exception('Division by zero.'));
+throw $e;
+throw ($e);
+throw new \Exception("Bye");
+throw (new \Exception("Bye"));
+
+if (!$var) {
+    throw new Exception('Division by zero.');
+    throw (new Exception('Division by zero.'));
+    throw $e;
+    throw ($e);
+    throw new \Exception("Bye");
+    throw (new \Exception("Bye"));
+}
