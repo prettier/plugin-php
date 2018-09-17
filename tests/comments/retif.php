94d85cceb8e6fd599c5b078d718a87ea6f0bad71
evilebottnawi <sheo13666q@gmail.com>
2018-09-17 19:47:28 +0300

    feat: better print comments in `retif` node

---
+++ tests/comments/retif.php
@@ -0,0 +1,29 @@
+<?php
+
+$inspect = $condition
+    ? // node <= 0.8.x
+    function($v, $colors) {
+        return $util->inspect($v, 0, 0, $colors);
+    }
+    : // node > 0.8.x
+    function($v, $colors) {
+        return $util->inspect($v, [ 'colors' => colors ]);
+    };
+
+$extractTextPluginOptions = $shouldUseRelativeAssetPaths
+    // Making sure that the publicPath goes back to to build folder.
+    ? [ 'publicPath' => (new Foo($cssFilename->split('/')->length))->join('../') ] :
+    [];
+
+$extractTextPluginOptions = $shouldUseRelativeAssetPaths
+    ? // Making sure that the publicPath goes back to to build folder.
+    [ 'publicPath' => (new Foo($cssFilename->split("/")->length))->join("../") ]
+    : [];
+
+$extractTextPluginOptions = $shouldUseRelativeAssetPaths // Making sure that the publicPath goes back to to build folder.
+    ? [ 'publicPath' => (new Foo($cssFilename->split("/")->length))->join("../") ]
+    : [];
+
+$var = $process->env->NODE_ENV === "production"
+    ? call("./configureProdStore") // a
+    : call("./configureDevStore"); // b
