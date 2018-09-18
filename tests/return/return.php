ac3fdfce1cb270ae03adadb00bd51f0a6812ef72
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-09-18 12:38:03 +0300

    feat: avoid parens in `return` node (#644)

--- tests/return/return.php
+++ tests/return/return.php
@@ -6,6 +6,12 @@ function sum($a, $b) {
 
 return;
 return $name;
+return
+
+$name;
+return
+
+$VeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongClassName;
 return [1, 2, 3];
 return [$veryVeryVeryVeryVeryVeryLongKey => 'VeryVeryVeryVeryVeryVeryVeryVeryVeryLongString', $veryVeryVeryVeryVeryVeryLongKey => 'VeryVeryVeryVeryVeryVeryVeryVeryVeryLongString', 3];
 return ($a + $b);
@@ -19,3 +25,42 @@ return (1000000000 + 1000000000 + 1000000000 + 1000000000 + 1000000000 + 1000000
 return $veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongVariable ? true : false;
 return $veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongVariable->veryVeryVeryVeryVeryVeryLongProperty;
 return ((($true ? ('foo' . ('foo'. 'bar')) : "foobarfoo")));
+return 'string' . 'string' . 'string' . 'string' . 'string' . 'string' . 'string' . 'string';
+return ('string' . 'string' . 'string' . 'string' . 'string' . 'string' . 'string' . 'string');
+
+return function () {};
+return (function () {})();
+
+return call();
+return $a->b->c->d->e->f;
+return $a->b()->c()->d()->e()->f();
+return call()->b()->c()->d()->e()->f();
+return $a::b()::c()::d()::e()::f();
+return Foo::a()::b()::c()::d()::e()::f();
+
+return "
+string
+string
+string
+";
+
+return $str = <<<EOD
+Example of string
+spanning multiple lines
+using heredoc syntax.
+EOD;
+
+return $str = <<<'EOD'
+Example of string
+spanning multiple lines
+using nowdoc syntax.
+EOD;
+
+return new class {};
+return new class {
+    public function foo() {
+        return 1;
+    }
+};
+
+return null;
