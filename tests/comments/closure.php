9f1bee5ccafd2ed54da7f2bd9297cdb04b790f79
evilebottnawi <sheo13666q@gmail.com>
2018-10-02 00:19:09 +0300

    test: comments inside `closure` body

--- tests/comments/closure.php
+++ tests/comments/closure.php
@@ -2,3 +2,26 @@
 
 $var = function (/* Comment */) {};
 $var = function (/* Comment */ $arg /* Comment */) {};
+
+$this->getProtocolsForPerson($person)
+    ->map(function ($protocol) {
+        //            $protocol->history_data = $this->getHistory(
+        //                $protocol->objeto_id
+        //            )->map(function ($history) {
+        //                $history->history_fields = $this->getHistoryFiles(
+        //                    $history->historicu_id
+        //                );
+        //
+        //                return $history;
+        //            });
+
+        return $protocol;
+    })
+    ->each(function ($protocol) {
+        $this->importProtocol($protocol);
+
+        $this->increment(
+            10,
+            "{$protocol->perssoa_name} ({$protocol->perssoa_id})"
+        );
+    });
