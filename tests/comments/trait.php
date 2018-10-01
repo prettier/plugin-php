61301197d16feb22234b09e8b05d28b636c33233
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-10-01 15:27:39 +0300

    refactor: trait comments tests (#690)

--- tests/comments/trait.php
+++ tests/comments/trait.php
@@ -13,3 +13,13 @@ trait tFoo extends /* Comment */ ReflectionMethod {}
 trait tFoo extends
     // Comment
     ReflectionMethod {}
+
+class ImplementingClass {
+    /* Comment */
+    /* Comment */ use /* Comment */ testTrait /* Comment */, /* Comment */ otherTrait /* Comment */;
+    /* Comment */ use /* Comment */ testTrait /* Comment */, /* Comment */ implementingTrait /* Comment */ { /* Comment */
+        /* Comment */ A/* Comment */::/* Comment */ testFunction /* Comment */ insteadof /* Comment */ C /* Comment */ ; /* Comment */
+        /* Comment */ B/* Comment */::/* Comment */ someOtherTestFunction /* Comment */ as /* Comment */ aliasedFunctionName /* Comment */; /* Comment */
+        /* Comment */ C/* Comment */::/* Comment */ testFunction /* Comment */ as  /* Comment */ private /* Comment */ testVisibility /* Comment */;
+    }
+}
