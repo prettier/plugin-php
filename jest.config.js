73121883f401764d90b609d946b64b9badcf318a
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-10-03 19:47:21 +0300

    chore: fix linting (#726)

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,21 @@
 const ENABLE_COVERAGE = !!process.env.CI;
 
 module.exports = {
-  displayName: "test",
-  setupFiles: ["<rootDir>/tests_config/run_spec.js"],
-  testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
-  snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
-  collectCoverage: ENABLE_COVERAGE,
-  collectCoverageFrom: ["src/**/*.js", "!<rootDir>/node_modules/"],
-  testEnvironment: "node"
+  projects: [
+    {
+      displayName: "test",
+      setupFiles: ["<rootDir>/tests_config/run_spec.js"],
+      testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
+      snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
+      collectCoverage: ENABLE_COVERAGE,
+      collectCoverageFrom: ["src/**/*.js", "!<rootDir>/node_modules/"],
+      testEnvironment: "node"
+    },
+    {
+      runner: "jest-runner-eslint",
+      displayName: "lint",
+      testMatch: ["<rootDir>/**/*.js"],
+      testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/coverage/"]
+    }
+  ]
 };
