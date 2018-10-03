f999a152ae635d398442df4413e1e7e86501c9d8
evilebottnawi <sheo13666q@gmail.com>
2018-10-03 14:41:40 +0300

    test: insert pragma CRLF

---
+++ tests/insert-pragma/pragma-sparsedocblock-LF.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This docblock doesn't have any pragma in it yet.
+ */
+
+echo "Hello World";
---
+++ tests/insert-pragma/pragma-withdocblock-LF.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This is a preexisting docblock which will receive a "format" pragma
+ *
+ * @author    Bob Bobberson
+ * @copyright Doge Inc. 2018 - All Rights Reserved
+ */
+
+echo "Hello World";
