d393cd9178eb8e8619f4939c66b39d241e9910dd
evilebottnawi <sheo13666q@gmail.com>
2018-10-03 17:25:30 +0300

    fix: do not mix LF and CRLF

--- src/pragma.js
+++ src/pragma.js
@@ -1,47 +1,66 @@
 "use strict";
 
 const parse = require("./parser");
-const { EOL } = require("os");
 const memoize = require("mem");
 
 const reHasPragma = /@prettier|@format/;
 
 const extractDocBlocks = memoize(text => {
   const parsed = parse(text);
+
   return parsed.comments.filter(el => el.kind === "commentblock");
 });
 
-const hasPragma = text => {
+function guessLineEnding(text) {
+  const index = text.indexOf("\n");
+
+  if (index >= 0 && text.charAt(index - 1) === "\r") {
+    return "\r\n";
+  }
+
+  return "\n";
+}
+
+function hasPragma(text) {
   // fast path optimization - check if the pragma shows up in the file at all
   if (!reHasPragma.test(text)) {
     return false;
   }
+
   const [firstDocBlock] = extractDocBlocks(text);
+
   if (firstDocBlock) {
     const { value } = firstDocBlock;
+
     return reHasPragma.test(value);
   }
-  return false;
-};
 
-const injectPragma = docblock => {
+  return false;
+}
+
+function injectPragma(docblock, text) {
   let lines = docblock.split(/\r?\n/g);
+
   if (lines.length === 1) {
     // normalize to multiline for simplicity
     const [, line] = /\/*\*\*(.*)\*\//.exec(lines[0]);
+
     lines = ["/**", ` * ${line.trim()}`, " */"];
   }
+
   // find the first @pragma
   // if there happens to be one on the opening line, just put it on the next line.
   const pragmaIndex = lines.findIndex(line => /@\S/.test(line)) || 1;
+
   // not found => index == -1, which conveniently will splice 1 from the end.
   lines.splice(pragmaIndex, 0, " * @format");
 
-  return lines.join(EOL);
-};
+  return lines.join(guessLineEnding(text));
+}
 
-const insertPragma = text => {
+function insertPragma(text) {
   const [firstDocBlock] = extractDocBlocks(text);
+
   if (firstDocBlock) {
     const {
       start: { offset: startOffset },
@@ -49,9 +68,12 @@ const insertPragma = text => {
     } = firstDocBlock.loc;
     const before = text.substring(0, startOffset);
     const after = text.substring(endOffset);
-    return `${before}${injectPragma(firstDocBlock.value)}${after}`;
+
+    return `${before}${injectPragma(firstDocBlock.value, text)}${after}`;
   }
+
   const openTag = "<?php";
+
   if (!text.startsWith(openTag)) {
     // bail out
     return text;
@@ -60,12 +82,13 @@ const insertPragma = text => {
   const splitAt = openTag.length;
   const phpTag = text.substring(0, splitAt);
   const after = text.substring(splitAt);
+
   return `${phpTag}
 /** 
  * @format 
  */
 ${after}`;
-};
+}
 
 module.exports = {
   hasPragma,
