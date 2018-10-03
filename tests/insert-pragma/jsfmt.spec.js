1e985482989843c7adf9501d2c8abccc3348fe08
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-10-03 14:46:59 +0300

    test: insert pragma CRLF (#715)

--- tests/insert-pragma/jsfmt.spec.js
+++ tests/insert-pragma/jsfmt.spec.js
@@ -1,1 +1,1 @@
-run_spec(__dirname, ["php"], { insertPragma: true });
+run_spec(__dirname, ["php"], { insertPragma: true, keepEOL: true });
---
+++ tests/insert-pragma/pragma-alreadyinserted-LF.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This already has the pragma, it shouldn't get another.
+ *
+ * @format
+ * @author    Bob Bobberson
+ * @copyright Doge Inc. 2018 - All Rights Reserved
+ */
+
+echo "Hello World";
