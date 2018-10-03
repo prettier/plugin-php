e93241a8cc493e4c2e3480d3ee614dc5b038b488
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-10-03 14:34:31 +0300

    test: require pragma with CRLF (#714)

---
+++ tests/require-pragma/file-with-alt-pragma-LF.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This works too
+ *
+ * @prettier
+ */
+
+echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "\n";
---
+++ tests/require-pragma/file-with-pragma-LF.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This turns on prettier formatting if --require-pragma is passed
+ *
+ * @format
+ */
+
+echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "\n";
