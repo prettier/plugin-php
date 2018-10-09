345580d2e08aeb70e3223534755b374c656c3547
evilebottnawi <sheo13666q@gmail.com>
2018-10-09 20:12:36 +0300

    fix: `declare` comments

---
+++ tests/comments/declare.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(ticks=1) {
+    // Comment
+}
+
+// Comment
+declare(ticks=1) {
+    // Comment
+    $a = 1;
+    // Comment
+}
+
+// Comment
+declare(ticks=1);
+// Comment
+
+// Comment
+declare(ticks=1):
+    // Comment
+    $test = 1;
+    // Comment
+enddeclare;
+// Comment
