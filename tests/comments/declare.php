7cbf6eed4bac592c726e388a6e9c49a996e12aff
evilebottnawi <sheo13666q@gmail.com>
2018-10-11 15:29:57 +0300

    refactor: `getCommentChildNodes` function

--- tests/comments/declare.php
+++ tests/comments/declare.php
@@ -22,3 +22,29 @@ declare(ticks=1):
     // Comment
 enddeclare;
 // Comment
+
+declare(ticks=1) // Comment
+{
+}
+
+declare(ticks=1) { // Comment
+}
+
+declare(ticks=1) // Comment
+{
+    $a = 1;
+}
+
+declare(ticks=1) { // Comment
+    $a = 1;
+}
+
+declare // Comment
+(ticks=1)
+{
+}
+
+declare /* Comment */ (/* Comment */ticks/* Comment */=/* Comment */1/* Comment */) {}
+
+declare // Comment
+(strict_types=1);
