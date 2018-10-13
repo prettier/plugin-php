7264d1105dba0cb34dc70d08f50ccee48acb3c4d
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-10-13 19:10:56 +0300

    test: variable variables (#791)

--- tests/variable/variable.php
+++ tests/variable/variable.php
@@ -49,3 +49,11 @@ $var = (Foo::bar())['baz']();
 // $var = (Foo::$bar)['baz'];
 
 $$$$$$$$$$var = "I like playing in the park";
+
+$var = ${$a[1]};
+$var = ${$a}[1];
+$var = ${"name_$type"};
+$var = ${ 'price_for_' . $today};
+${date("M")} = "Worked";
+${$a}[] =  "four" ;
+${$base.$suffix} = "whatever";
