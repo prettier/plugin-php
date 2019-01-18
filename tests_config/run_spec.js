ccd334c4ec2a378e35bb63c6c2d22417b7bf937b
evilebottnawi <sheo13666q@gmail.com>
2019-01-18 23:19:46 +0300

    refactor: test utils

--- tests_config/run_spec.js
+++ tests_config/run_spec.js
@@ -62,19 +62,39 @@ global.run_spec = function run_spec(dirname, parsers, options) {
         parser: parsers[0]
       });
 
+      const hasEndOfLine = "endOfLine" in mainOptions;
+
       const output = format(input, filename, mainOptions);
+      const visualizedOutput = visualizeEndOfLine(output);
 
       test(basename, () => {
-        expect(raw(`${source}\n${output}`)).toMatchSnapshot();
+        expect(visualizedOutput).toEqual(
+          visualizeEndOfLine(consistentEndOfLine(output))
+        );
+        expect(
+          raw(
+            createSnapshot(
+              hasEndOfLine
+                ? visualizeEndOfLine(
+                    text
+                      .replace(RANGE_START_PLACEHOLDER, "")
+                      .replace(RANGE_END_PLACEHOLDER, "")
+                  )
+                : source,
+              hasEndOfLine ? visualizedOutput : output,
+              Object.assign({}, baseOptions, { parsers })
+            )
+          )
+        ).toMatchSnapshot();
       });
 
-      parsers.slice(1).forEach(parser => {
+      for (const parser of parsers.slice(1)) {
         const verifyOptions = Object.assign({}, baseOptions, { parser });
         test(`${basename} - ${parser}-verify`, () => {
           const verifyOutput = format(input, filename, verifyOptions);
-          expect(output).toEqual(verifyOutput);
+          expect(visualizedOutput).toEqual(visualizeEndOfLine(verifyOutput));
         });
-      });
+      }
 
       // this will only work for php tests (since we're in the php repo)
       if (AST_COMPARE && parsers[0] === "php") {
@@ -119,6 +139,90 @@ function format(source, filename, options) {
     : result.formatted;
 }
 
+function consistentEndOfLine(text) {
+  let firstEndOfLine;
+  return text.replace(/\r\n?|\n/g, endOfLine => {
+    if (!firstEndOfLine) {
+      firstEndOfLine = endOfLine;
+    }
+    return firstEndOfLine;
+  });
+}
+
+function visualizeEndOfLine(text) {
+  return text.replace(/\r\n?|\n/g, endOfLine => {
+    switch (endOfLine) {
+      case "\n":
+        return "<LF>\n";
+      case "\r\n":
+        return "<CRLF>\n";
+      case "\r":
+        return "<CR>\n";
+      default:
+        throw new Error(`Unexpected end of line ${JSON.stringify(endOfLine)}`);
+    }
+  });
+}
+
+function createSnapshot(input, output, options) {
+  const separatorWidth = 80;
+  const printWidthIndicator =
+    options.printWidth > 0 && Number.isFinite(options.printWidth)
+      ? `${" ".repeat(options.printWidth)}| printWidth`
+      : [];
+
+  return []
+    .concat(
+      printSeparator(separatorWidth, "options"),
+      printOptions(
+        omit(
+          options,
+          k => k === "rangeStart" || k === "rangeEnd" || k === "cursorOffset"
+        )
+      ),
+      printWidthIndicator,
+      printSeparator(separatorWidth, "input"),
+      input,
+      printSeparator(separatorWidth, "output"),
+      output,
+      printSeparator(separatorWidth)
+    )
+    .join("\n");
+}
+
+function printSeparator(width, description) {
+  description = description || "";
+  const leftLength = Math.floor((width - description.length) / 2);
+  const rightLength = width - leftLength - description.length;
+  return "=".repeat(leftLength) + description + "=".repeat(rightLength);
+}
+
+function printOptions(options) {
+  const keys = Object.keys(options).sort();
+  return keys.map(key => `${key}: ${stringify(options[key])}`).join("\n");
+  function stringify(value) {
+    return value === Infinity
+      ? "Infinity"
+      : Array.isArray(value)
+      ? `[${value.map(v => JSON.stringify(v)).join(", ")}]`
+      : JSON.stringify(value);
+  }
+}
+
+function omit(obj, fn) {
+  return Object.keys(obj).reduce((reduced, key) => {
+    if (key === "plugins") {
+      return reduced;
+    }
+
+    const value = obj[key];
+    if (!fn(key, value)) {
+      reduced[key] = value;
+    }
+    return reduced;
+  }, {});
+}
+
 /**
  * Wraps a string in a marker object that is used by `./raw-serializer.js` to
  * directly print that string in a snapshot without escaping all double quotes.
@@ -129,13 +233,4 @@ function raw(string) {
     throw new Error("Raw snapshots have to be strings.");
   }
   return { [Symbol.for("raw")]: string };
-}
-
-function mergeDefaultOptions(parserConfig) {
-  return Object.assign(
-    {
-      printWidth: 80
-    },
-    parserConfig
-  );
 }
