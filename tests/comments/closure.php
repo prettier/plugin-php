eedf84b0b6d2efb3704dc590366c5af95523cfb3
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-01-18 16:24:38 +0300

    test: for comments in closure (#884)

--- tests/comments/closure.php
+++ tests/comments/closure.php
@@ -18,3 +18,19 @@ $var = function () {
     // this is a function
     return 42;
 };
+
+$this
+    // Comment
+    ->map(function ($protocol) {
+        // Comment
+
+        return $protocol;
+    })
+    // Comment
+    ->
+    // Comment
+    call(function ($arg) {
+        // Comment
+
+        return $arg;
+    });
