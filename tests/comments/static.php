ec4b8f9fe623c171f7b0120cab78312e85258ef9
evilebottnawi <sheo13666q@gmail.com>
2019-01-23 20:35:00 +0300

    test: comments in static node

--- tests/comments/static.php
+++ tests/comments/static.php
@@ -3,3 +3,12 @@ static /* Comment */ $a /* Comment */, /* Comment */ $b /* Comment */;
 
 static // Comment
 $a;
+
+// Comment
+static
+    // Comment
+    $a // Comment
+    // Comment
+    = // Comment
+    // Comment
+    2021;
