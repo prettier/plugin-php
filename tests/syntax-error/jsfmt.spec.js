e159be980ba6f198eb36a2b94f4d4f0ee9321b6b
Loilo <florian.reuschel@gmx.de>
2019-02-14 01:59:12 +0100

    Replace accidental protected space

--- tests/syntax-error/jsfmt.spec.js
+++ tests/syntax-error/jsfmt.spec.js
@@ -16,7 +16,7 @@ test("Syntax errors have the expected structure", () => {
   try {
     raiseSyntaxError();
   } catch (err) {
-    // Convert error to plain object since additional properties are not snapshotted otherwise
+    // Convert error to plain object since additional properties are not snapshotted otherwise
     const errObject = Object.assign({}, err, {
       // Strip ANSI from code frame since not all test environments may support it
       codeFrame: stripAnsi(err.codeFrame)
