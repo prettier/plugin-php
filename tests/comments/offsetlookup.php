d24e5c22bf897650f8c8c286814695faea6287b4
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-08-26 17:40:34 +0300

    fix: update php parser (#1134)

--- tests/comments/offsetlookup.php
+++ tests/comments/offsetlookup.php
@@ -1,3 +1,13 @@
 <?php
 
 echo /*Comment*/$a/*Comment*/[/*Comment*/1/*Comment*/]/*Comment*/;
+
+$var = $var
+    // Comment
+    ['foo'] // Comment
+    // Comment
+    ['bar'] // Comment
+    // Comment
+    ['baz'] // Comment
+    // Comment
+    ['qqq'];
