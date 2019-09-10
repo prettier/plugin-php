ac0511ef794e59ee6206f048f7552092f022639b
evilebottnawi <sheo13666q@gmail.com>
2019-09-10 21:18:42 +0300

    test: assignref

--- tests/assignref/assignref.php
+++ tests/assignref/assignref.php
@@ -7,3 +7,25 @@ class Foo {
         $var = &self::$darwinCache[$k];
     }
 }
+
+$a = &$b / 100;
+
+$var = $arr['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'];
+$var = &$arr['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'];
+$var = $arr['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'];
+$var = &$arr['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'];
+$variable['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'] = $variable->registered[1];
+$variable['veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongString'] = &$variable->registered[1];
+
+$obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = $obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = $obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = $veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongValue;
+$obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = $obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = $obj->veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongProperty = &$veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongValue;
+
+$test =
+    $superSupersuperSupersuperSupersuperSupersuperSuperLong
+    ->exampleOfOrderOfGetterAndSetterReordered;
+$test =
+    &$superSupersuperSupersuperSupersuperSupersuperSuperLong
+    ->exampleOfOrderOfGetterAndSetterReordered;
+
+$this->long->expression->before->array->statesArray[$state->getCirculationStateId()] = $state->getName();
+$this->long->expression->before->array->statesArray[$state->getCirculationStateId()] = &$state->getName();
