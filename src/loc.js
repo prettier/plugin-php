f5eb3137121c17e5f5967908703617e6bba6a475
fisker <lionkay@gmail.com>
2023-08-28 17:58:53 +0800

    Use optional chaining

--- src/loc.js
+++ src/loc.js
@@ -1,6 +1,4 @@
-const loc = (prop) => (node) => {
-  return node.loc && node.loc[prop] && node.loc[prop].offset;
-};
+const loc = (prop) => (node) => node.loc?.[prop]?.offset;
 
 export const locStart = loc("start");
 export const locEnd = loc("end");
