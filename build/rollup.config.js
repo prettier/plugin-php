d7f6aa3e8e58ad68fddd7ceb5cddf0ad7e0168f3
fisker <lionkay@gmail.com>
2023-08-31 10:38:05 +0800

    Update build script

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,21 +1,18 @@
-import { resolve, dirname } from "path";
 import url from "url";
 
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import alias from "rollup-plugin-alias";
-import inject from "rollup-plugin-inject";
-import replace from "rollup-plugin-replace";
-import babel from "rollup-plugin-babel";
-import json from "rollup-plugin-json";
-import { terser } from "rollup-plugin-terser";
+import alias from "@rollup/plugin-alias";
+import inject from "@rollup/plugin-inject";
+import replace from "@rollup/plugin-replace";
+import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
-const __dirname = dirname(url.fileURLToPath(import.meta.url));
-const SRC_DIR = resolve(__dirname, "..", "src");
-const BUILD_DIR = resolve(__dirname, "..", "build");
+const getPath = (file) => url.fileURLToPath(new URL(file, import.meta.url));
 
 export default {
-  input: resolve(SRC_DIR, "index.js"),
+  input: getPath("../src/index.js"),
   output: {
     file: "standalone.js",
     format: "umd",
@@ -35,18 +32,18 @@ export default {
     }),
     commonjs(),
     alias({
-      entries: [
-        { find: "assert", replacement: resolve(BUILD_DIR, "shims/assert.js") },
-      ],
+      entries: [{ find: "assert", replacement: getPath("./shims/assert.js") }],
     }),
     inject({
-      Buffer: resolve(BUILD_DIR, "shims/buffer.js"),
+      Buffer: getPath("./shims/buffer.js"),
     }),
     replace({
+      preventAssignment: true,
       "process.arch": JSON.stringify("x32"),
     }),
     json(),
     babel({
+      babelHelpers: "bundled",
       babelrc: false,
       plugins: [],
       compact: false,
