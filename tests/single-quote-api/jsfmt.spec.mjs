ea27073b441a1959f957457786347c39faa80745
Shuhei Hayashibara <meikyowise@gmail.com>
2024-02-03 22:33:08 +0900

    fix: 🐛 singleQuote option does not work on format API (#2303)
    * fix: 🐛 singleQuote option does not work on format API
    
    * test: 💍 add test for singleQuote option from api call
    
    * refactor: 💡 add comment
    
    * style: 💄 apply formatter

---
+++ tests/single-quote-api/jsfmt.spec.mjs
@@ -0,0 +1,21 @@
+import prettier from "prettier/standalone";
+import * as prettierPluginPhp from "../../src/index.mjs";
+
+// https://github.com/prettier/plugin-php/issues/2302
+test(`singleQuote option on format api`, async () => {
+  const input = `<?php echo link_to_route("frontend.users.user.show", $users["name"], $users['_id']); ?>`;
+  const result = await prettier.format(input, {
+    plugins: [prettierPluginPhp],
+    singleQuote: true,
+    parser: "php",
+  });
+
+  const expected = `<?php echo link_to_route(
+    'frontend.users.user.show',
+    $users['name'],
+    $users['_id']
+); ?>
+`;
+
+  expect(result).toEqual(expected);
+});
