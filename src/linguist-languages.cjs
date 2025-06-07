f69b73690ba881ed03f05b57e2c9683363e59ab5
fisker <lionkay@gmail.com>
2025-06-07 17:47:05 +0800

    Update `linguist-languages` to v8

--- src/linguist-languages.cjs
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-module.exports.LINGUIST_LANGUAGES_PHP = require("linguist-languages/data/PHP");
-module.exports.LINGUIST_LANGUAGES_HTML_PHP = require("linguist-languages/data/HTML+PHP");
